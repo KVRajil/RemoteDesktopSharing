24c185d0d0cf6390c86c2bb36504489b3b89e75d
rajil kv <rajilkv@live.com>
2014-10-19 12:38:31 +0530

    no message

---
+++ RDViewerServer/src/com/server/utils/MessageInfo.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.server.utils;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author SAFI
+ */
+public class MessageInfo implements Serializable {
+
+    private String ipAddress = "";
+    private String subject = "";
+    private Date time = null;
+    private String messageBody = "";
+
+    /**
+     * @return the ipAddress
+     */
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    /**
+     * @param ipAddress the ipAddress to set
+     */
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    /**
+     * @return the subject
+     */
+    public String getSubject() {
+        return subject;
+    }
+
+    /**
+     * @param subject the subject to set
+     */
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    /**
+     * @return the time
+     */
+    public Date getTime() {
+        return time;
+    }
+
+    /**
+     * @param time the time to set
+     */
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    /**
+     * @return the messageBody
+     */
+    public String getMessageBody() {
+        return messageBody;
+    }
+
+    /**
+     * @param messageBody the messageBody to set
+     */
+    public void setMessageBody(String messageBody) {
+        this.messageBody = messageBody;
+    }
+}
